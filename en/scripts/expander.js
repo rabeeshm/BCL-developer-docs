6a0d9973a6359ecf2670a899365d2699f71e02f9
Matt Boles <mboles@brightcove.com>
2014-09-23 16:51:45 -0400

    added expander functionality for Robert to approve

---
+++ en/scripts/expander.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#expanderHead").click(function(){
+    $("#expanderContent").slideToggle();
+    if ($("#expanderSign").text() == "+"){
+      $("#expanderSign").html("−")
+    }
+    else {
+      $("#expanderSign").text("+")
+    }
+  });
+});
