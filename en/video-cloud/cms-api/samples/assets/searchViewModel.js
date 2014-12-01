70dbf0b927918b39a97d0e58d44aad9297ebb382
rcrooks <rcrooks@brightcove.com>
2014-12-01 11:45:59 -0500

    nav update -- aapi

---
+++ en/video-cloud/cms-api/samples/assets/searchViewModel.js
@@ -0,0 +1,7 @@
+searchViewModel = function () {
+    self.proxyURL = "//solutions.brightcove.com:8006",
+    self.client_id = ,
+    self.client_secret = ,
+    self.baseURL = "https://cms.api.brightcove.com/v1beta1/accounts/",
+    self.url = ,
+}
