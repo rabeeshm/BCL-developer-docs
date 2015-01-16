8cae1ec49ff793f1ec1b88096c1903211f9681b4
rcrooks <rcrooks@brightcove.com>
2015-01-16 09:03:58 -0500

    tweak to oauth error message reference

--- en/video-cloud/oauth-api/reference/assets/error-messages.js
+++ en/video-cloud/oauth-api/reference/assets/error-messages.js
@@ -4,14 +4,14 @@ var errorMessages = {
         "unsupported-response-type": "The \"response_type\" parameter is missing or invalid.",
         "invalid-client": "The \"client_id\" parameter is missing, does not name a client registration that is applicable for the requested call, or is not properly authenticated.",
         "unauthorized-client": "The client registration named by the \"client_id\" parameter is not allowed to perform the requested operation.",
-        "invalid-request": "The following parameters of the request are missing, syntactically incorrect, or inconsistent with one another %s",
+        "invalid-request": "The following parameters of the request are missing, syntactically incorrect, or inconsistent with one another [parameters]",
         "invalid-scope": "The \"scope\" parameter is invalid or not permitted by the named client registration.",
         "access-denied": "The request does not carry authentication credentials sufficient to authorize the requested access.",
         "invalid-grant": "The provided authorization grant value is expired, revoked, or otherwise invalid.",
         "server-error": "An internal server error occurred."
     },
     "client-registration":    {
-        "invalid-request": "The following parameters of the request are missing, syntactically incorrect, or inconsistent with one another %s",
+        "invalid-request": "The following parameters of the request are missing, syntactically incorrect, or inconsistent with one another [parameters]",
         "invalid-request-no-update": "No attributes were supplied to update.",
         "endpoint-not-allowed": "The requested scope may not be accessed through this API endpoint.",
         "access-denied": "The request does not carry authentication credentials sufficient to authorize the requested access.",
