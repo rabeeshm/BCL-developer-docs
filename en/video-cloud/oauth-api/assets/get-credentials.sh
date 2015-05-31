0f34387aada487001277e14d74dc33fdb39ad028
rcrooks <rcrooks@brightcove.com>
2015-05-31 14:50:57 -0400

    work on credentials script

--- en/video-cloud/oauth-api/assets/get-credentials.sh
+++ en/video-cloud/oauth-api/assets/get-credentials.sh
@@ -8,35 +8,12 @@ echo 'Enter your Video Cloud account id:'
 read ACCOUNT_ID
 echo Your account id: $ACCOUNT_ID
 echo --------------------------
-echo 'Enter the full API call:'
-read API_CALL
-echo Your API call: $API_CALL
+export API_CALL="https://oauth.brightcove.com/v3/client_credentials"
+echo Here is the API POST request: $API_CALL
 echo --------------------------
-echo "Enter the HTTP method: [ ${bold}g${normal} (GET - default) | ${bold}po${normal} (POST) | ${bold}pa${normal} (PATCH) | ${bold}pu${normal} (PUT) | ${bold}d${normal} (DELETE) ]:"
-read VERB
-if [ "$VERB" = "" ]
-	then
-	export VERB="GET"
-elif [ "$VERB" = "g" ] || [ "$VERB" = "GET" ]
-	then
-	export VERB="GET"
-elif [ "$VERB" = "po" ] || [ "$VERB" = "p" ] || [ "$VERB" = "POST" ]
-	then
-	export VERB="POST"
-elif [ "$VERB" = "pa" ] || [ "$VERB" = "PATCH" ]
-	then
-	export VERB="PATCH"
-elif [ "$VERB" = "pu" ] || [ "$VERB" = "PUT" ]
-	then
-	export VERB="PUT"
-elif [ "$VERB" = "d" ] || [ "$VERB" = "DELETE" ]
-	then
-	export VERB="DELETE"
-fi
-echo "Your request type: $VERB"
-echo --------------------------
-echo 'Enter data to be submitted in the request body:'
-read DATA
+export VERB="POST"
+export DATA='name=ingest-profiles-api-credential&maximum_scope=[{"identity":{"type":"video-cloud-account","account_id":"$ACCOUNT_ID"},\
+"operations":[]}'
 echo Your call verb: $DATA
 echo --------------------------
 TOKEN=$(curl -s --data "grant_type=client_credentials" https://oauth.brightcove.com/v3/access_token --header "Content-Type: application/x-www-form-urlencoded" --user "$CLIENT_ID:$CLIENT_SECRET" | sed -E 's/.*access_token\"\:\"([^\"]+)\".*/\1/');
