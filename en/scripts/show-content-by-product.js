5f0e211589cc58ebe8c05a03f78777703ee831f3
rcrooks <rcrooks@brightcove.com>
2015-07-22 15:32:16 -0400

    product content script

--- en/scripts/show-content-by-product.js
+++ en/scripts/show-content-by-product.js
@@ -1,9 +1,18 @@
 var BCLSshowContentByProduct = ( function (window, document) {
     var path = window.location.href,
-        vcContent = document.getElementsByClassName('video-cloud-only'),
-        performContent = document.getElementsByClassName('perform-only'),
         hideContent,
-        doesContain;
+        doesContain,
+        hasClass;
+
+    /**
+     * determines whether element has a class
+     * @param {object|node} elem the element
+     * @param {string} cls the name of the class you’re looking for
+     * @return {boolean} true if the element has that class, false if not
+     */
+    function hasClass( elem, cls ) {
+        return (" " + elem.className + " " ).indexOf( “ " + cls + " " ) > -1;
+    }
 
     /**
      * determines if one string contains another string
@@ -22,21 +31,24 @@ var BCLSshowContentByProduct = ( function (window, document) {
      * sets style attribute display:none for each element in a nodeset
      * @param  {array|collection of nodes} nodeSet [description]
      */
-    hideContent = function(nodeSet) {
-        var i, iMax, item;
+    hideContent = function(excludeClass) {
+        var i, iMax, item,
+            nodeSet = document.querySelectorAll('a, div, h1, h2, h3, h4, h5, p, span, pre, code, ul, ol, li, img, table, thead, tbody, tr, th, td, dl, dt, dd');
 
         iMax = nodeSet.length;
         for (i = 0; i < iMax; i++) {
             item = nodeSet[i];
-            item.setAttribute('style', 'display:none')
+            if (hasClass(item, excludeClass)) {
+                item.setAttribute('style', 'display:none');
+            }
         }
     }
 
     // if product = perform, hide video cloud content and vice-versa
     if (doesContain(path, 'perform')) {
-        hideContent(vcContent);
+        hideContent('video-cloud-only');
     } elseif (doesContain(path, 'video-cloud')) {
-        hideContent(performContent);
+        hideContent('perform');
     }
 
 })(window, document);
