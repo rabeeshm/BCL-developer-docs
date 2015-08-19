b04d90415bbc01fc4799e5bdedd7611c7e973f34
rcrooks <rcrooks@brightcove.com>
2015-08-19 08:46:18 -0400

    file cleanup

--- en/video-cloud/ingest-profiles-api/getting-started/assets/profiles-curl.57838016001.sh
+++
@@ -1,4 +0,0 @@
-token=$(curl -s -k --user 5l5Gfy_xdccStmVAGF8RGUadBgk28HCqpQk9MxwRg9BZmVaiN5DtfcTPotxDaFwNyOfAN4Vu-L5Qe3bHc4n28w -d grant_type=client_credentials https://oauth.brightcove.com/v2/access_token | cut -d'"' -f4)
-curl -H "Content-Type: application/json" -H "Authorization: Bearer $token" $*
-echo
-
--- en/video-cloud/ingest-profiles-api/getting-started/assets/profiles-curl.921483702001.sh
+++
@@ -1,4 +0,0 @@
-token=$(curl -s -k --user f2499b79-9b4d-44bd-ba91-8c0223a7a918:qVAO__Wok5lnSO0KHJn_pa3yxQmfcawrx1UEZXbO0tL9NKwHUr53OEEPSiO6XYkSPxCjbvaPwld0629KlX0Mdw -d grant_type=client_credentials https://oauth.brightcove.com/v2/access_token | cut -d'"' -f4)
-curl -H "Content-Type: application/json" -H "Authorization: Bearer $token" $*
-echo
-
