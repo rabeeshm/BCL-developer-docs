625b6b0696d111931196dcaede0cf835318e8e92
rcrooks <rcrooks@brightcove.com>
2016-04-03 08:48:00 -0400

    WIP player sample

--- en/video-cloud/brightcove-player/samples/assets/enlarge-player/enlarge-player.js
+++ en/video-cloud/brightcove-player/samples/assets/enlarge-player/enlarge-player.js
@@ -3,26 +3,32 @@ videojs.plugin('enlargePlayer', function() {
         largeWidth = 640,
         largeHeight = 360,
         smallWidth = 272,
-        smallHeight = 153;
+        smallHeight = 153,
+        inpagePlayerWrapper = document.getElementById('inpagePlayerWrapper');
 
 
     function makePlayerLarge() {
-        player.dimensions(largeWidth, largeHeight);
+        console.log('make large');
+        inpagePlayerWrapper.setAttribute('style', 'width:' + largeWidth + 'px;height:' + largeHeight + 'px');
     }
 
     function makePlayerSmall() {
-        player.dimensions(smallWidth, smallHeight);
+        console.log('make small');
+        inpagePlayerWrapper.setAttribute('style', 'width:' + smallWidth + 'px;height:' + smallHeight + 'px;');
     }
 
     function mouseoutOn() {
         player.on('mouseout', makePlayerSmall);
+        makePlayerSmall();
     }
 
     function mouseoutOff() {
         player.off('mouseout', makePlayerSmall);
     }
 
-    player.on('mouseover', makePlayerLarge);
+    makePlayerSmall();
+    inpagePlayerWrapper.addEventListener('mouseover', makePlayerLarge);
+    inpagePlayerWrapper.addEventListener('mouseout', makePlayerSmall);
     player.on('play', mouseoutOff);
     player.on('ended', mouseoutOn);
     mouseoutOn();
