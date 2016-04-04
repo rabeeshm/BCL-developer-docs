95c1aea67d72b3d9056390dfe1047ea98ccd5c47
rcrooks <rcrooks@brightcove.com>
2016-04-04 05:55:21 -0400

    player sample WIP

--- en/video-cloud/brightcove-player/samples/assets/enlarge-player/enlarge-player.js
+++ en/video-cloud/brightcove-player/samples/assets/enlarge-player/enlarge-player.js
@@ -1,18 +1,26 @@
 videojs.plugin('enlargePlayer', function() {
     var player = this,
+        playerEl = player.el(),
+        playerParent = playerEl.parentNode,
         largeWidth = 640,
         largeHeight = 360,
         smallWidth = 272,
         smallHeight = 153,
-        playerWrapper = document.getElementById('playerWrapper');
+        wrapper = document.createElement('div');
+
+    playerWrapper.setAttribute('id', 'playerWrapper');
+    playerParent.insertBefore(wrapper, playerEl);
+    wrapper.appendChild(playerEl);
+    playerWrapper = document.getElementById('playerWrapper');
+
 
 
     function makePlayerLarge() {
-        playerWrapper.setAttribute('style', 'width:' + largeWidth + 'px;height:' + largeHeight + 'px;transition: width .3s ease-in-out, height .3s ease-in-out;');
+        playerWrapper.setAttribute('style', 'width:' + largeWidth + 'px;height:' + largeHeight + 'px;transition: width .4s ease-in-out, height .4s ease-in-out;');
     }
 
     function makePlayerSmall() {
-        playerWrapper.setAttribute('style', 'width:' + smallWidth + 'px;height:' + smallHeight + 'px;transition: width .3s ease-in-out, height .3s ease-in-out;');
+        playerWrapper.setAttribute('style', 'width:' + smallWidth + 'px;height:' + smallHeight + 'px;transition: width .4s ease-in-out, height .4s ease-in-out;');
     }
 
     function mouseoutOn() {
