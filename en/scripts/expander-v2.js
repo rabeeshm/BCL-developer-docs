163b8b4657cf3f50ae3d17a79d13b469ae28a1cc
rcrooks <rcrooks@brightcove.com>
2016-05-18 12:13:49 -0400

    template stuff

--- en/scripts/expander-v2.js
+++ en/scripts/expander-v2.js
@@ -1,10 +1,21 @@
-var BCLS = ( function () {
-  var expanderHeads = document.getElementsByClassName('bcls-expander-head');
+var BCLS = (function() {
+    var expanderHeads = document.getElementsByClassName('bcls-expander-head'),
+        i,
+        iMax;
+    iMax = expanderHeads.length;
+
+    function toggleBody(evt) {
+        
+    }
+
+    for (i = 0; i < iMax; i++) {
+        expanderHeads[i].addEventListener('click', toggleBody);
+    }
 })();
 
-$(document).ready(function(){
-  $("#expanderHead").click(function(){
-    $("#expanderContent").slideToggle();
-    $("#expanderHead").toggleClass("changed");
-  });
+$(document).ready(function() {
+    $("#expanderHead").click(function() {
+        $("#expanderContent").slideToggle();
+        $("#expanderHead").toggleClass("changed");
+    });
 });
