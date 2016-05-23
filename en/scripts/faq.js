98516900c7f1f15f6ad52e2208879498dd987e7c
rcrooks <rcrooks@brightcove.com>
2016-05-23 09:57:14 -0400

    template tweaks

--- en/scripts/faq.js
+++ en/scripts/faq.js
@@ -1,41 +1,39 @@
 var BCLS_faq = (function (window, document) {
-    "use strict";
+    'use strict';
     var // elements
-        questions = document.getElementsByClassName("bcls-question"),
-        answers = document.getElementsByClassName("bcls-answer");
+        questions = document.getElementsByClassName('bcls-question'),
+        answers = document.getElementsByClassName('bcls-answer');
     function showAnswer(evt) {
-        var answerNumber = parseInt(evt.target.id.substring(1)),
+        var answerNumber = parseInt(this.id.substring(1)),
             i = 0,
-            j = answers.length;
-        logit("evt", evt);
-        logit("answerNumber", answerNumber);
+            iMax = answers.length;
         // hide all answers except the one for the selected question
-        for (i = 0; i < j; i++) {
+        for (i = 0; i < iMax; i++) {
             if (i === answerNumber) {
-                answers[i].style.display = "block";
+                answers[i].style.display = 'block';
             } else {
-                answers[i].style.display = "none";
+                answers[i].style.display = 'none';
             }
         }
     }
     function init() {
-        var i = 0, j = 0;
+        var i = 0, iMax = 0;
         if (questions) {
-            j = questions.length;
+            iMax = questions.length;
         }
 
         // add IDs
-        for (i = 0; i < j; i++) {
+        for (i = 0; i < iMax; i++) {
             // set ids for answers
-            answers[i].setAttribute("id", ("a" + i.toString()));
+            answers[i].setAttribute('id', ('a' + i.toString()));
             // hide all answers initially
-            answers[i].setAttribute("style", "display:none");
+            answers[i].setAttribute('style', 'display:none');
             // set ids for questions
-            questions[i].setAttribute("id", ("q" + i.toString()));
+            questions[i].setAttribute('id', ('q' + i.toString()));
             // add styling to questions
-            questions[i].setAttribute("style", "margin-top:1em;margin-bottom:1em;");
+            questions[i].setAttribute('style', 'margin-top:1em;margin-bottom:1em;');
             // add event listeners
-            questions[i].addEventListener("click", showAnswer);
+            questions[i].addEventListener('click', showAnswer);
         }
     }
     // call init to kick things off
