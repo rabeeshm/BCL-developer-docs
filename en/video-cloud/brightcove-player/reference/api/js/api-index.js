5e72d92193518ae9486489f2c56f153168f70e50
Matthew Boles <mboles@brightcove.com>
2016-08-15 13:42:45 -0400

    Update docs to Brightcove Player 5.7.1

--- en/video-cloud/brightcove-player/reference/api/js/api-index.js
+++ en/video-cloud/brightcove-player/reference/api/js/api-index.js
@@ -15,7 +15,7 @@ var BCLSVJS = (function (window, document, docData) {
         bclslog,
         addHeaderContent,
         addIndex,
-        playerVersion = '5.6.2',
+        playerVersion = '5.7.1',
         init;
     /**
      * Logging function - safe for IE
