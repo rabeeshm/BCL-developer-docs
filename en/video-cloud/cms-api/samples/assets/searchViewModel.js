013cb5d7acbddbf06a706f82f5aab3b7627cb26e
rcrooks <rcrooks@brightcove.com>
2016-09-19 11:28:59 -0400

    moving assets

--- en/video-cloud/cms-api/samples/assets/searchViewModel.js
+++
@@ -1,7 +0,0 @@
-function cmsSearchModel () {
-	this.client_id: ko.observable(""),
-	this.client_secret: ko.observable(""),
-	this.account_id: ko.observable(""),
-    this.proxyURL = ko.observable("//solutions.brightcove.com:8006"),
-    this.baseURL = ko.observable("https://cms.api.brightcove.com/v1beta1/accounts/"),
-}
