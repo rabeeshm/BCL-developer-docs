f6fe9161ef0c8a8d2b1447c9b864609c2c4b7753
Robert Crooks <rcrooks@brightcove.com>
2018-06-06 11:27:44 -0400

    update postfix script

--- swagger/postfix.js
+++ swagger/postfix.js
@@ -1,20 +1,21 @@
-var BCLS = ( function (window, document) {
+function postfix() {
   var topbar_wrapper = document.getElementsByClassName('topbar-wrapper')[0],
-  img = document.createElement('img'),
-  aDocs = document.createElement('a'),
-  aHome = document.createElement('a'),
-  txt = document.createTextNode(' | '),
-  frag = document.createDocumentFragment();
-  img.setAttribute('src', 'https://learning-services-media.brightcove.com/doc-assets/general/images/bc-logo-ondark.png');
+    img = document.createElement('img'),
+    aDocs = document.createElement('a'),
+    aHome = document.createElement('a'),
+    frag = document.createDocumentFragment();
+  img.setAttribute('src', 'https://learning-services-media.brightcove.com/doc-assets/general/images/bc-logo-ondark-small.png');
   img.setAttribute('alt', 'Brightcove');
+  img.setAttribute('class', 'topbar-img');
   aDocs.setAttribute('href', 'https://support.brightcove.com');
+  aDocs.setAttribute('class', 'topbar-a');
   aDocs.textContent = 'Brightcove Docs';
-  aHome.setAttribute('href', '../index.html');
+  aHome.setAttribute('href', 'https://docs.brightcove.com/index.html');
+  aHome.setAttribute('class', 'topbar-a');
   aHome.textContent = 'API References Index';
   topbar_wrapper.innerHTML = '';
   frag.appendChild(img);
   frag.appendChild(aHome);
-  frag.appendChild(txt);
   frag.appendChild(aDocs);
   topbar_wrapper.appendChild(frag);
-})(window, document);
+}
