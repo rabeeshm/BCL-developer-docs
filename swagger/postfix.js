a3bf3802877ebffef8124659068c2f107834fd0f
rcrooks <rcrooks@brightcove.com>
2018-06-06 14:36:33 -0400

    update postfix scipt for exand-all button

--- swagger/postfix.js
+++ swagger/postfix.js
@@ -1,9 +1,17 @@
 function postfix() {
   var topbar_wrapper = document.getElementsByClassName('topbar-wrapper')[0],
+    opblocks = document.getElementsByClassName('opblock'),
+    model_containers = document.getElementsByClassName('model-container'),
+    description_div = document.getElementsByClassName('description')[0],
+    btn = document.createElement('button'),
+    txt = ' (click to make the entire doc searchable)',
+    span = document.createElement(span),
+    expand_all,
     img = document.createElement('img'),
     aDocs = document.createElement('a'),
     aHome = document.createElement('a'),
     frag = document.createDocumentFragment();
+  // add custom header
   img.setAttribute('src', 'https://learning-services-media.brightcove.com/doc-assets/general/images/bc-logo-ondark-small.png');
   img.setAttribute('alt', 'Brightcove');
   img.setAttribute('class', 'topbar-img');
@@ -18,4 +26,23 @@ function postfix() {
   frag.appendChild(aHome);
   frag.appendChild(aDocs);
   topbar_wrapper.appendChild(frag);
+  // add expand all button
+  btn.setAttribute('id', 'expand_all');
+  btn.setAttribute('class', 'btn try-out__btn');
+  btn.textContent = 'Expand All Sections';
+  span.textContent = txt;
+  description_div.appendChild(btn);
+  description_div.appendChild(span);
+  expand_all = document.getElementById('expand_all');
+  expand_all.addEventListener('click', function() {
+    var i, iMax;
+    iMax = opblocks.length;
+    for (i = 0; i < iMax; i++) {
+      opblocks[i].classList.add('is-open');
+    }
+    iMax = model_containers.length;
+    for (i = 0; i < iMax; i++) {
+      model_containers[i].classList.add('is-open');
+    }
+  });
 }
