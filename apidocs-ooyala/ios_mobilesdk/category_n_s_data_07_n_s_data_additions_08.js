9fed2db3c2278b85e9ed8943f2e7eeb6909a39a2
Robert Crooks <rcrooks@brightcove.com>
2019-04-03 12:29:12 -0400

    ios sdk 2

---
+++ apidocs-ooyala/ios_mobilesdk/category_n_s_data_07_n_s_data_additions_08.js
@@ -0,0 +1,10 @@
+var category_n_s_data_07_n_s_data_additions_08 =
+[
+    [ "base64Encoding", "category_n_s_data_07_n_s_data_additions_08.html#ad1727e2f594cad1b56e1e405524bfc09", null ],
+    [ "base64EncodingWithLineLength:", "category_n_s_data_07_n_s_data_additions_08.html#aebeb4e9b80809f8a50051b054f998b93", null ],
+    [ "dataWithBase64EncodedString:", "category_n_s_data_07_n_s_data_additions_08.html#a7ea1d9027d77f68091c2167ffe5d9e7d", null ],
+    [ "dataWithUncompressedContentsOfFile:", "category_n_s_data_07_n_s_data_additions_08.html#a3ffac2e28c19071d1d5b669cebc1ed53", null ],
+    [ "gzipDeflate", "category_n_s_data_07_n_s_data_additions_08.html#ae825b8b20a4a547929ab6f9a25d787b8", null ],
+    [ "gzipInflate", "category_n_s_data_07_n_s_data_additions_08.html#a378e7398b5fce07f46946a9b25efe5c0", null ],
+    [ "initWithBase64EncodedString:", "category_n_s_data_07_n_s_data_additions_08.html#a06601e9b3858c1b7f4bfe872cb77af59", null ]
+];
---
+++ apidocs-ooyala/ios_mobilesdk/category_o_o_ooyala_player_07_ad_plugin_management_08.js
@@ -0,0 +1,5 @@
+var category_o_o_ooyala_player_07_ad_plugin_management_08 =
+[
+    [ "createStateNotifier", "category_o_o_ooyala_player_07_ad_plugin_management_08.html#ad8285ad074dd4fc3a7b653e25f260d64", null ],
+    [ "registerAdPlayer:forType:", "category_o_o_ooyala_player_07_ad_plugin_management_08.html#a40ef2e3522774ecb88bf039a3a9276cc", null ]
+];
