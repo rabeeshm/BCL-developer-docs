037dba97e90b6ffe3cf944264392482d07b1acae
Robert Crooks <rcrooks@brightcove.com>
2019-04-03 12:37:09 -0400

    5

---
+++ apidocs-ooyala/ios_mobilesdk/freewheel_sdk/html/hierarchy.js
@@ -0,0 +1,34 @@
+var hierarchy =
+[
+    [ "<CLLocationManagerDelegate>", null, [
+      [ "OOFreewheelManager", "interface_o_o_freewheel_manager.html", null ]
+    ] ],
+    [ "<FWNotificationContext>", "protocol_f_w_notification_context-p.html", null ],
+    [ "<NSObject>", null, [
+      [ "<FWAdInstance >", "protocol_f_w_ad_instance_01-p.html", null ],
+      [ "<FWAdManager >", "protocol_f_w_ad_manager_01-p.html", null ],
+      [ "<FWContext >", "protocol_f_w_context_01-p.html", null ],
+      [ "<FWCreativeRendition >", "protocol_f_w_creative_rendition_01-p.html", null ],
+      [ "<FWCreativeRenditionAsset >", "protocol_f_w_creative_rendition_asset_01-p.html", null ],
+      [ "<FWExtension >", "protocol_f_w_extension_01-p.html", null ],
+      [ "<FWRenderer >", "protocol_f_w_renderer_01-p.html", null ],
+      [ "<FWRendererController >", "protocol_f_w_renderer_controller_01-p.html", null ],
+      [ "<FWSlot >", "protocol_f_w_slot_01-p.html", null ]
+    ] ],
+    [ "OOAdMoviePlayer", null, [
+      [ "OOFreewheelAdPlayer", "interface_o_o_freewheel_ad_player.html", null ]
+    ] ],
+    [ "OOAdSpot", null, [
+      [ "OOFreewheelAdSpot", "interface_o_o_freewheel_ad_spot.html", null ]
+    ] ],
+    [ "OOAdSpotPlugin", null, [
+      [ "OOFreewheelManager", "interface_o_o_freewheel_manager.html", null ]
+    ] ],
+    [ "<OOAdSpotPluginDelegate>", null, [
+      [ "OOFreewheelManager", "interface_o_o_freewheel_manager.html", null ]
+    ] ],
+    [ "OOFreewheelManager(Internal)", "category_o_o_freewheel_manager_07_internal_08.html", null ],
+    [ "<UIApplicationDelegate>", null, [
+      [ "OOFreewheelManager", "interface_o_o_freewheel_manager.html", null ]
+    ] ]
+];
