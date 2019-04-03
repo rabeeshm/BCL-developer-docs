2f23ff5e6b0fd91ff0ce3473dc6566b243cb1902
Robert Crooks <rcrooks@brightcove.com>
2019-04-03 12:49:30 -0400

    release jp

---
+++ apidocs-ooyala/release-jp/toctree.js
@@ -0,0 +1,45 @@
+
+      var tree;
+      
+      function treeInit() {
+      tree = new YAHOO.widget.TreeView("treeDiv1");
+      var root = tree.getRoot();
+    
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+ var objd4e36 = { label: "Subscribe to Ooyala Release Notes", href:"http://help.ooyala.com/video-platform/concepts/release_notes_subscribe.html", target:"contentwin" };
+    var d4e36 = new YAHOO.widget.TextNode(objd4e36, root, false);
+
+ var objd4e42 = { label: "2017 API文書リリースノート", href:"api_relnotes/release_notes_2017.html", target:"contentwin" };
+    var d4e42 = new YAHOO.widget.TextNode(objd4e42, root, false);var objd4e49 = { label: "2017-05-24リリース", href:"api_relnotes/2017-05-24.html", target:"contentwin" };
+    var d4e49 = new YAHOO.widget.TextNode(objd4e49, d4e42, false);var objd4e56 = { label: "2017-03-29リリース", href:"api_relnotes/2017-03-29.html", target:"contentwin" };
+    var d4e56 = new YAHOO.widget.TextNode(objd4e56, d4e42, false);var objd4e63 = { label: "2017-03-22リリース", href:"api_relnotes/2017-03-22.html", target:"contentwin" };
+    var d4e63 = new YAHOO.widget.TextNode(objd4e63, d4e42, false);var objd4e70 = { label: "2017-03-13リリース", href:"api_relnotes/2017-03-13.html", target:"contentwin" };
+    var d4e70 = new YAHOO.widget.TextNode(objd4e70, d4e42, false);var objd4e77 = { label: "2017-02-28リリース", href:"api_relnotes/2017-02-28.html", target:"contentwin" };
+    var d4e77 = new YAHOO.widget.TextNode(objd4e77, d4e42, false);var objd4e84 = { label: "2017-02-21リリース", href:"api_relnotes/2017-02-21.html", target:"contentwin" };
+    var d4e84 = new YAHOO.widget.TextNode(objd4e84, d4e42, false);var objd4e91 = { label: "2017-02-17リリース", href:"api_relnotes/2017-02-17.html", target:"contentwin" };
+    var d4e91 = new YAHOO.widget.TextNode(objd4e91, d4e42, false);var objd4e98 = { label: "2017-02-07リリース", href:"api_relnotes/2017-02-07.html", target:"contentwin" };
+    var d4e98 = new YAHOO.widget.TextNode(objd4e98, d4e42, false);var objd4e105 = { label: "2017-01-27リリース", href:"api_relnotes/2017-01-27.html", target:"contentwin" };
+    var d4e105 = new YAHOO.widget.TextNode(objd4e105, d4e42, false);var objd4e112 = { label: "2017-01-11リリース", href:"api_relnotes/2017-01-11.html", target:"contentwin" };
+    var d4e112 = new YAHOO.widget.TextNode(objd4e112, d4e42, false);
+
+ var objd4e120 = { label: "2016 API文書リリースノート", href:"api_relnotes/release_notes_2016.html", target:"contentwin" };
+    var d4e120 = new YAHOO.widget.TextNode(objd4e120, root, false);var objd4e127 = { label: "2016-12-21リリース", href:"api_relnotes/2016-12-21.html", target:"contentwin" };
+    var d4e127 = new YAHOO.widget.TextNode(objd4e127, d4e120, false);var objd4e134 = { label: "2016-12-19リリース", href:"api_relnotes/2016-12-19.html", target:"contentwin" };
+    var d4e134 = new YAHOO.widget.TextNode(objd4e134, d4e120, false);var objd4e141 = { label: "2016-10-14リリース", href:"api_relnotes/2016-10-14.html", target:"contentwin" };
+    var d4e141 = new YAHOO.widget.TextNode(objd4e141, d4e120, false);var objd4e148 = { label: "2016-08-24リリース", href:"api_relnotes/2016-08-24.html", target:"contentwin" };
+    var d4e148 = new YAHOO.widget.TextNode(objd4e148, d4e120, false);var objd4e155 = { label: "2016-08-18リリース", href:"api_relnotes/2016-08-18.html", target:"contentwin" };
+    var d4e155 = new YAHOO.widget.TextNode(objd4e155, d4e120, false); 
+  
+
+      tree.draw(); 
+      } 
+      
+      YAHOO.util.Event.addListener(window, "load", treeInit); 
+    
