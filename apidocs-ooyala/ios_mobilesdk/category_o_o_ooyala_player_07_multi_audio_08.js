83d435f07d8c62914389208dbb6a66dd67d778e2
Robert Crooks <rcrooks@brightcove.com>
2019-04-03 12:33:06 -0400

    3
    3

---
+++ apidocs-ooyala/ios_mobilesdk/category_o_o_ooyala_player_07_multi_audio_08.js
@@ -0,0 +1,4 @@
+var category_o_o_ooyala_player_07_multi_audio_08 =
+[
+    [ "hasMultipleAudioTracks", "category_o_o_ooyala_player_07_multi_audio_08.html#a38d81ab0fde84098aaac5c95b0fd5d8b", null ]
+];
---
+++ apidocs-ooyala/ios_mobilesdk/freewheel_sdk/html/annotated_dup.js
@@ -0,0 +1,17 @@
+var annotated_dup =
+[
+    [ "<FWAdInstance >", "protocol_f_w_ad_instance_01-p.html", "protocol_f_w_ad_instance_01-p" ],
+    [ "<FWAdManager >", "protocol_f_w_ad_manager_01-p.html", "protocol_f_w_ad_manager_01-p" ],
+    [ "<FWContext >", "protocol_f_w_context_01-p.html", "protocol_f_w_context_01-p" ],
+    [ "<FWCreativeRendition >", "protocol_f_w_creative_rendition_01-p.html", "protocol_f_w_creative_rendition_01-p" ],
+    [ "<FWCreativeRenditionAsset >", "protocol_f_w_creative_rendition_asset_01-p.html", "protocol_f_w_creative_rendition_asset_01-p" ],
+    [ "<FWExtension >", "protocol_f_w_extension_01-p.html", "protocol_f_w_extension_01-p" ],
+    [ "<FWNotificationContext>", "protocol_f_w_notification_context-p.html", "protocol_f_w_notification_context-p" ],
+    [ "<FWRenderer >", "protocol_f_w_renderer_01-p.html", "protocol_f_w_renderer_01-p" ],
+    [ "<FWRendererController >", "protocol_f_w_renderer_controller_01-p.html", "protocol_f_w_renderer_controller_01-p" ],
+    [ "<FWSlot >", "protocol_f_w_slot_01-p.html", "protocol_f_w_slot_01-p" ],
+    [ "OOFreewheelAdPlayer", "interface_o_o_freewheel_ad_player.html", "interface_o_o_freewheel_ad_player" ],
+    [ "OOFreewheelAdSpot", "interface_o_o_freewheel_ad_spot.html", "interface_o_o_freewheel_ad_spot" ],
+    [ "OOFreewheelManager", "interface_o_o_freewheel_manager.html", "interface_o_o_freewheel_manager" ],
+    [ "OOFreewheelManager(Internal)", "category_o_o_freewheel_manager_07_internal_08.html", "category_o_o_freewheel_manager_07_internal_08" ]
+];
