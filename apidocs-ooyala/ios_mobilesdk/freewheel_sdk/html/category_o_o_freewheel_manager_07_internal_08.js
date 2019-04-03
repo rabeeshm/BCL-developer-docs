dcb436b6e1ac2437d3a05a078df58ba914696cf7
Robert Crooks <rcrooks@brightcove.com>
2019-04-03 12:34:38 -0400

    4

---
+++ apidocs-ooyala/ios_mobilesdk/freewheel_sdk/html/category_o_o_freewheel_manager_07_internal_08.js
@@ -0,0 +1,7 @@
+var category_o_o_freewheel_manager_07_internal_08 =
+[
+    [ "adsPlaying", "category_o_o_freewheel_manager_07_internal_08.html#af922dfc0a374a9ded4ba152c0f2890d8", null ],
+    [ "onAdCompleted", "category_o_o_freewheel_manager_07_internal_08.html#aa9aeabb5c6e12946b09b662c0add35ec", null ],
+    [ "showAdLearnMoreButton", "category_o_o_freewheel_manager_07_internal_08.html#a484c7d7539fa73e6b809e402aa3c0990", null ],
+    [ "showAdsControls", "category_o_o_freewheel_manager_07_internal_08.html#abff6aa39db2fd712d480a3e610c52636", null ]
+];
