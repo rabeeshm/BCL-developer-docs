83e0da7ee57910df66534f2986936d44a48aa167
Robert Crooks <rcrooks@brightcove.com>
2019-04-04 09:07:56 -0400

    about link

--- apidocs-ooyala/content_protection_api/authorization_api/Contents/searchBox.js
+++ apidocs-ooyala/content_protection_api/authorization_api/Contents/searchBox.js
@@ -568,7 +568,7 @@ function copyRAMLFunction() {
     footerString += "<a href=\"https://www.brightcove.com/en/legal/privacy\" target=\"_blank\"> Website Privacy Policy &bull;</a>";
     footerString += "<a href=\"https://www.brightcove.com/en/terms-and-conditions\" target=\"_blank\"> Terms of Service &bull;</a>";
     footerString += " All Rights Reserved ";
-    footerString += "<a href=\"http://support.ooyala.com/developers/documentation/concepts/about.html\" target=\"_blank\">";
+    footerString += "<a href=\"https://help-ooyala.brightcove.com/developers/documentation/concepts/about.html\" target=\"_blank\">";
     footerString += "&bull; Ooyala Support - Developer Documentation</a>";
     copyNotice.innerHTML = footerString;
     
