366570c933e1c447c4cd339f98e1bacd2e61221a
Robert Crooks <rcrooks@brightcove.com>
2019-04-23 11:06:09 -0400

    redirect script for apidoc pages

--- js/apidoc-redirect.js
+++ js/apidoc-redirect.js
@@ -2,4 +2,10 @@ var BCLSredirect = ( function (window, document) {
   var href = window.location.href,
     newHref = href.replace('apidoc.html', index.html),
     ts;
+
+    function redirect() {
+      window.location.href = newHref;
+    }
+
+    ts = window.setTimeout(redirect, 5000);
 })(window, document);
